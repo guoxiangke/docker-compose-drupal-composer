13d40d1c2b8444f8019b3afa51f60492ca9622a6
xiangkeguo <xiangkeguo@linkdoc.com>
2018-06-02 17:37:04 +0800

    ok

--- auto.sh
+++ auto.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 pwd
+gtp && gtps
 cp -vvv ./html/composer.json ./drupal/composer.json
+cd ./drupal
+gtp
+gtac 'update composer.json'
+gtps
+ssh root@123.207.178.25<< EOF
+cd /root/html/dcdc_api/ && git checkout && git pull && chmod +x ./run.sh && ./run.sh
+EOF
---
+++ run.sh
@@ -0,0 +1,42 @@
+	#! /bin/bash
+	if [ -z "$1" ]
+	then
+	     ProjectName="ddApi"
+	else
+		ProjectName=$1
+	fi
+	if [ -z "$2" ]
+	then
+	     GitUrl="https://github.com/guoxiangke/dcdc_api.git"
+	else
+		GitUrl=$2
+	fi
+
+	if [ -z "$3" ]
+	then
+	    GitBranch="master"
+	else
+		GitBranch=$3
+	fi
+	echo Your ProjectName is : $ProjectName
+	echo your github url $GitUrl
+	echo your github Branch must be $GitBranch
+	cd ./drupal
+	if [ -d .git ]; then
+		git checkout .
+		git fetch
+		git checkout $GitBranch
+		git pull origin $GitBranch
+		cd ../
+	else
+		cd ../
+		rm ./code -rf
+		git clone -b $GitBranch $GitUrl ./code
+	fi;
+	
+	
+	#todo if drupal updates : docker build -t="drupal:composer" .
+	docker-compose -p $ProjectName down --remove-orphans
+	docker-compose -p $ProjectName  up -d  --build --remove-orphans --force-recreate
+
+	docker restart a-nginx a-nginx-gen
