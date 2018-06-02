b09902de2288b2b6368b3e0796dbf46e91d7c6ac
xiangkeguo <xiangkeguo@linkdoc.com>
2018-06-02 17:47:13 +0800

    auto commit

--- auto.sh
+++ auto.sh
@@ -4,5 +4,5 @@ git pull && git commit -am 'auto commit' && git push
 cp -vvv ./html/composer.json ./drupal/composer.json
 cd ./drupal && git pull && git commit -am 'auto commit composer.json' && git push
 ssh root@123.207.178.25<< EOF
-cd /root/html/dcdc_api/ && git checkout && git pull && chmod +x ./run.sh && ./run.sh
+cd /root/html/dcdc_api/ && git checkout . && git pull && chmod +x ./run.sh && ./run.sh
 EOF
